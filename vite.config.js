0067f202387a9b95f0c21bb779d0de0c6f568fd8
Saikiran R Baddi <saikiranrbc@gmail.com>
2026-08-08 22:08:25 +0530

    Purge_stale_redirects_and_force_emptyOutDir

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
     host: true
